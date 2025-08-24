f77fcb40d42abca7c027d07269f961ec20f0d794
Johnny <johnnync13@gmail.com>
2025-08-24 11:46:46 +0200

    Use AVBufferSinkParams and scan stream side data

--- src/video/ffmpeg/filter_graph.cc
+++ src/video/ffmpeg/filter_graph.cc
@@ -63,14 +63,12 @@ void FFMPEGFilterGraph::Init(std::string filters_descr, AVCodecContext *dec_ctx)
     // LOG(INFO) << "create filter src";
 
     /* buffer video sink: to terminate the filter chain. */
-	// buffersink_params = av_buffersink_params_alloc();
-	// buffersink_params->pixel_fmts = pix_fmts;
-	CHECK_GE(avfilter_graph_create_filter(&buffersink_ctx_, buffersink, "out",
-		NULL, NULL, filter_graph_.get()), 0) << "Cannot create buffer sink";
-	// av_free(buffersink_params);
+        AVBufferSinkParams *buffersink_params = av_buffersink_params_alloc();
+        buffersink_params->pixel_fmts = pix_fmts;
+        CHECK_GE(avfilter_graph_create_filter(&buffersink_ctx_, buffersink, "out",
+                NULL, buffersink_params, filter_graph_.get()), 0) << "Cannot create buffer sink";
+        av_free(buffersink_params);
     // LOG(INFO) << "create filter sink";
-    // CHECK_GE(av_opt_set_bin(buffersink_ctx_, "pix_fmts", (uint8_t *)&pix_fmts, sizeof(AV_PIX_FMT_RGB24), AV_OPT_SEARCH_CHILDREN), 0) << "Set bin error";
-    CHECK_GE(av_opt_set_int_list(buffersink_ctx_, "pix_fmts", pix_fmts, AV_PIX_FMT_NONE, AV_OPT_SEARCH_CHILDREN), 0) << "Set output pixel format error.";
 
     // LOG(INFO) << "create filter set opt";
     /* Endpoints for the filter graph. */
